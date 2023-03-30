d5802216faf58055a98963cf88f7bca81479dc79
ilk07 <ilk07@inbox.ru>
2023-03-31 02:12:47 +0300

    add userlist to repositry, add field for authorities to user class

--- src/main/java/ru/hw/restauthservice/repository/UserRepository.java
+++ src/main/java/ru/hw/restauthservice/repository/UserRepository.java
@@ -2,15 +2,27 @@ package ru.hw.restauthservice.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.hw.restauthservice.model.Authorities;
-import java.util.*;
+import ru.hw.restauthservice.model.User;
+
+import java.util.Collections;
+import java.util.List;
 
 @Repository
 public class UserRepository {
-    public List<Authorities> getUserAuthorities(String user, String password) {
-        if (user.equals("admin") && password.equals("pass")) {
-            return Arrays.asList(Authorities.READ,Authorities.WRITE, Authorities.DELETE);
-        }
-        return Collections.emptyList();
+    private List<User> usersList;
+
+    public UserRepository() {
+        this.usersList = List.of(
+                new User("John", "Doe24", List.of(Authorities.DELETE, Authorities.READ, Authorities.WRITE)),
+                new User("Mike", "miki56747", List.of(Authorities.WRITE)),
+                new User("Sam", "samy4", List.of(Authorities.READ)),
+                new User("Anna", "anna12", List.of(Authorities.READ, Authorities.WRITE))
+        );
+    }
+
+    public List<Authorities> getUserAuthorities(String name, String password) {
+        return usersList.stream().filter(user -> user.getUser().equals(name) && user.getPassword().equals(password)).findFirst().map(User::getAuthorities).orElse(Collections.emptyList());
+
     }
 
 }
